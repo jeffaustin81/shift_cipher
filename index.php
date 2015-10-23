7caeb1b1d719293ac9d347519c8c96cecf845c07
Jeff Austin <jeffaustin81@gmail.com>
2015-10-23 12:01:48 -0700

    Add initial setup for form submition

--- index.php
+++ index.php
@@ -14,6 +14,11 @@
 /**
  * Root directory of Drupal installation.
  */
+
+ error_reporting(E_ALL);
+ ini_set('display_errors', TRUE);
+ ini_set('display_startup_errors', TRUE);
+
 define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
